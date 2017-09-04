be5f9c4938ceb6fe0775500ee15f70475eccc615
mordenius <mordenius@gmail.com>
2017-09-04 17:52:34 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+	"parser": "babel-eslint",
+	"parserOptions": {
+		"ecmaVersion": 6,
+		"sourceType": "module"
+	},
+	"rules": {
+		"indent": [2, "tab", {"SwitchCase": 1}],
+		"import/no-unresolved": 0,
+		"import/extensions": 0,
+		"linebreak-style": 0,
+		"radix": [2, "as-needed"]
+	}
+};
